f270379178c57d5f6563c5ed85f895b4da03b9d5
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-10 17:27:54 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const express=require("express");
 const {PORT}=require("../src/config/serverConfig");
 const {sendBasicEmail}=require("../src/services/email-service");
+const cron = require("node-cron");
 const app=express();
 
 const setupAndStartServer=()=>{
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
-        sendBasicEmail(
+     
+       cron.schedule('*/15 * * * *',()=>{
+     sendBasicEmail(
             'support@gmail.com',
             'abc@gmail.com',
             'This is a testing email',
             'Hey, how are you, I hope you like the support'
         );
     })
+    });
+   
+    
 }
 
 setupAndStartServer();
