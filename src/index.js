547f587dd0a0c53870e831e00e00e4ca2c93ca96
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-07-12 01:00:17 +0530

    rabbit MQ setup done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-const express=require("express");
-const {PORT}=require("../src/config/serverConfig");
-const {sendBasicEmail}=require("../src/services/email-service");
-const jobs=require("../src/utils/job");
-const TicketController=require("../src/controllers/ticket-controller");
-const app=express();
+const express = require("express");
+const { PORT } = require("../src/config/serverConfig");
+const jobs = require("../src/utils/job");
+const TicketController = require("../src/controllers/ticket-controller");
+const { createChannel } = require("../src/utils/messageQueue");
+const app = express();
 
-const setupAndStartServer=()=>{
-    app.use(express.json());
-        app.use(express.urlencoded({ extended: true }));
-    app.post('/api/v1/tickets',TicketController.create);
-    app.listen(PORT,()=>{
-        console.log(`Server started at ${PORT}`);
-        jobs();
-     
-       
-    });
-   
-    
-}
+const setupAndStartServer = async () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.post("/api/v1/tickets", TicketController.create);
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+    jobs();
+  });
+};
 
 setupAndStartServer();
